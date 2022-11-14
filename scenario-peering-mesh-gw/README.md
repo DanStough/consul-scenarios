b9c319af4ae50145e5b4d8b2c997cc63729f9504
DanStough <dan.stough@hashicorp.com>
2022-11-14 11:20:14 -0500

    final peering meshgw demo polish

--- scenario-peering-mesh-gw/README.md
+++ scenario-peering-mesh-gw/README.md
@@ -4,6 +4,7 @@ This scenario was design to be executed manually for demonstration.
 It comes in three parts:
 1. Run `run.sh`, which setup clusters and mesh gateways
 1. Manually execute the steps after `exit 0` in the `run.sh` to configure and peer the clusters 
+    1. You should see 2 total connections and 1 active in the envoy cluster metrics
 1. Run `validate.sh` to setup exported services and sidecars.
 
 If you `curl localhost:1234`, you can verify the exported service still works as expected in this arrangement.
--- scenario-peering-mesh-gw/run.sh
+++ scenario-peering-mesh-gw/run.sh
@@ -14,29 +14,15 @@ consul agent -dev -log-level trace -config-file config_dc2.hcl 1>./logs/dc2.log 
 echo "Sleeping for 10 seconds"
 sleep 30
 
-echo "Enable Control Plane Traffic"
-consul config write mesh.hcl
-CONSUL_HTTP_ADDR=localhost:9500 consul config write mesh.hcl
-
 echo "Starting Gateways"
 consul connect envoy -gateway mesh -service dc1-mesh-gateway -address "{{ GetInterfaceIP \"lo0\" }}:8443" -grpc-addr localhost:8502 -admin-bind 0.0.0.0:19004 -register -- --log-level info --log-path ./logs/dc1_meshgw_logs.txt 1>./logs/dc1-consul-sidecar.log &
 CONSUL_HTTP_ADDR=localhost:9500 consul connect envoy -gateway mesh -service dc2-mesh-gateway -address "{{ GetInterfaceIP \"lo0\" }}:9443" -grpc-addr localhost:9502 -admin-bind 0.0.0.0:19005 -register -- --log-level info --log-path ./logs/dc2_meshgw_logs.txt 1>./logs/dc2-consul-sidecar.log  &
 echo "waiting for gateways to start..."
 sleep 5
 
-
-
-consul peering generate-token -name accepting-dc2 > before_peering_token.txt
-CONSUL_HTTP_ADDR=localhost:9500  consul peering establish -name dialing-dc1 -peering-token "$(cat before_peering_token.txt)"
-
 exit 0
 
-
-
-
-
 # SCRIPT
-
 
 # Enable Control Plane Traffic
 consul config write mesh.hcl
@@ -46,15 +32,14 @@ CONSUL_HTTP_ADDR=localhost:9500 consul config write mesh.hcl
 # For the mesh gateway to have outputbound connectivity, the peering must be an outbound connection
 # for the cluster.
 echo "Establishing Peering"
-consul peering generate-token -name accepting-dc2 > before_peering_token.txt
+consul peering generate-token -name accepting-dc2 > peering_token.txt
+CONSUL_HTTP_ADDR=localhost:9500  consul peering establish -name dialing-dc1 -peering-token "$(cat peering_token.txt)"
 
 # Get unique serverName used as SNI
-ACCEPTING_SNI=$(cat before_peering_token.txt | base64 --decode | jq -r .ServerName)
-
-CONSUL_HTTP_ADDR=localhost:9500  consul peering establish -name dialing-dc1 -peering-token "$(cat before_peering_token.txt)"
+ACCEPTING_SNI=$(cat peering_token.txt | base64 --decode | jq -r .ServerName)
 
 # verify traffic 
-cat ./logs/dc2_meshgw_logs.txt | grep $ACCEPTING_SNI
+curl http://localhost:19005/clusters | grep "$ACCEPTING_SNI"
 
 # Cleanup
 # pkill node consul envoy
